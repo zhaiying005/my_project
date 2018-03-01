c1692728722c1c33781a9850223c00cb4af7e4e8
ying zhai <zhaiying005@gmail.com>
2018-03-01 10:16:17 -0800

    create .py script

---
+++ untitled0.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  1 10:05:33 2018
+
+@author: yingzhai
+"""
+
+import pandas as pd
+dat = pd.read_csv('data/gapminder_gdp_europe.csv')
